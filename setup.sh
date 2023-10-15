af4af6096c706533bfeca587fc397337a4c2e9ad
Belinda <belindadunu24@gmail.com>
2023-10-15 10:19:36 +0000

    modify files for Jenkins build

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 python3.7 -m venv test
 source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
-cd c4_deployment-5
+git clone https://github.com/belindadunu/Deployment_5.git
+cd Deployment_5
 pip install -r requirements.txt
 pip install gunicorn
 python database.py
